6ededc25bb52841bec1513e3277e3b3559d231ba
Libertocrat <libertocrat@proton.me>
2024-07-10 17:37:58 -0400

    Add: Code test cases for read, update, delete and list_all

--- tests/test_models.py
+++ tests/test_models.py
@@ -106,3 +106,76 @@ class TestProductModel(unittest.TestCase):
     # ADD YOUR TEST CASES HERE
     #
 
+    def test_read_a_product(self):
+        """It should Read a Product from the database"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(product.id)
+        # Fetch newly created product
+        new_product = Product.find(product.id)
+        # Assert that red product matches the one that was just created
+        self.assertEqual(new_product.name, product.name)
+        self.assertEqual(new_product.description, product.description)
+        self.assertEqual(Decimal(new_product.price), product.price)
+        self.assertEqual(new_product.available, product.available)
+        self.assertEqual(new_product.category, product.category)
+
+    def test_update_a_product(self):
+        """It should Update a Product"""
+        # Create a Product using ProductFactory
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        # Assert that it was assigned an id and shows up in the database
+        new_id = product.id
+        self.assertIsNotNone(new_id)
+        # Update description
+        new_description = "This is an updated description XYAJSD76872"
+        product.description = new_description
+        # Update product in database
+        product.update()
+        # Assert that the id is the same as the original
+        self.assertEqual(product.id, new_id)
+        # Assert that description has been successfully updated
+        self.assertEqual(product.description, new_description)
+
+        # Fetch all products in the database
+        all_products = Product.all()
+        # Assert there's only 1 product
+        self.assertEqual(len(all_products), 1)
+
+        # Fetch updated product
+        fetched_product = all_products[0]
+        # Assert that the id is the same as the original
+        self.assertEqual(fetched_product.id, new_id)
+        # Assert that description has been successfully updated
+        self.assertEqual(fetched_product.description, new_description)
+
+    def test_delete_a_product(self):
+        """It should Delete a Product"""
+        product = ProductFactory()
+        product.create()
+        # Call the create() method on the product to save it to the database.
+        # Assert  if the length of the list returned by Product.all() is equal to 1, to verify that after creating a product and saving it to the database, there is only one product in the system.
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        # Call the delete() method on the product object, to remove the product from the database.
+        product.delete()
+        # Assert if the length of the list returned by Product.all() is now equal to 0, indicating that the product has been successfully deleted from the database.
+        self.assertEqual(len(Product.all()), 0)
+
+    def test_list_all_products(self):
+        """It should List all Products in the database"""
+        products = Product.all()
+        # Assert if the products list is empty, indicating that there are no products in the database at the beginning of the test case.
+        self.assertEqual(len(products), 0)
+        # Use for loop to create five Product objects using a ProductFactory() and call the create() method on each product to save them to the database.
+        for _ in range(5):
+            product = ProductFactory()
+            product.create()
+        # Fetch all products from the database again using product.all()
+        all_products = Product.all()
+        # Assert if the length of the products list is equal to 5, to verify that the five products created in the previous step have been successfully added to the database.
+        self.assertEqual(len(all_products), 5)
